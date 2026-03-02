62aca8b6c83e2536216228c065fdd2ff13fa408b
Haochen Zhang <hz3396@nyu.edu>
2026-03-02 16:14:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,6 @@ from sklearn.metrics import mean_absolute_error, r2_score
 # App setup
 st.set_page_config(page_title="Deutsche Bahn Delay Project", layout="wide")
 st.title("🚆 Deutsche Bahn Train Departure Delay Predictor")
-st.write("Intro project: predict **departure_delay_m** with **Linear Regression**.")
 
 # Load data
 DATA_URL = "https://github.com/hz3396/DBDelayPredict/releases/download/v1.0/db_sample.csv"
